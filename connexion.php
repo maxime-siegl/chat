9ce189351f596da95d4120b47909d5cc1febc6de
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-10 16:38:07 +0100

    Création de classes

--- connexion.php
+++ connexion.php
@@ -11,12 +11,12 @@
     <form class="form_connexion" action="connexion.php" method="post">
       <div class="label_input">
         <label for="email">Adresse e-mail </label>
-        <input type="email" name="email" id="email" placeholder="Saisir son adresse e-mail" required>
+        <input type="email" name="email" id="email" required>
       </div>
 
       <div class="label_input">
         <label for="password">Mot de passe</label>
-        <input type="password" id="password" name="password" placeholder="Saisir son mot de passe" required>
+        <input type="password" id="password" name="password" required>
       </div>
 
       <button type="submit" class="button_pages"> Connexion </button>
