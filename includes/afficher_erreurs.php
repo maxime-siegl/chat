6f7ce40fb275e6d4f4a5380311a1dbe4d21c2c56
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-11 15:32:32 +0100

    Conditions de vérification des input

--- includes/afficher_erreurs.php
+++ includes/afficher_erreurs.php
@@ -10,15 +10,34 @@ if(empty($_POST['email']))
   $erreur = new Erreur($bdd);
   $erreur->afficherErreur($empty);
 
-}elseif(empty($_POST['password'])){
+}elseif(empty($_POST['password']))
+{
   $empty = "empty_password";
   $erreur = new Erreur($bdd);
   $erreur->afficherErreur($empty);
 
-}elseif(empty($_POST['confirm_password'])){
+}elseif(empty($_POST['confirm_password']))
+{
   $empty = "empty_confirm";
   $erreur = new Erreur($bdd);
   $erreur->afficherErreur($empty);
 
+}elseif($_POST['password'] != $_POST['confirm_password'])
+{
+  $error = "error_password";
+  $erreur = new Erreur($bdd);
+  $erreur->afficherErreur($error);
+
+}elseif(strlen($_POST['password']) < 8)
+{
+  $short = "short_password";
+  $erreur = new Erreur($bdd);
+  $erreur->afficherErreur($short);
+  
+}elseif(strlen($_POST['pseudo']) < 8)
+{
+  $short = "short_pseudo";
+  $erreur = new Erreur($bdd);
+  $erreur->afficherErreur($short);
 }
  ?>
