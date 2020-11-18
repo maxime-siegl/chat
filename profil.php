24a5a1ce3c446f199f01cafa1970e91900fa75d2
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-18 17:54:31 +0100

    Suppression include

--- profil.php
+++ profil.php
@@ -1,7 +1,5 @@
  <?php
     session_start();
-    #var_dump($_SESSION);
-    #var_dump(unserialize($_SESSION["utilisateur"]));
     include('ressources/php/bdd.php');
     include('classes/Utilisateur.php');
     include('classes/Erreur.php');
@@ -25,43 +23,43 @@
    <body class="page_index">
     <?php
       include 'views/header.php';
-      include("ressources/php/modifier_profil.php");
       ?>
 
      <main class="img_main">
        <div class="erreur hidden">
        </div>
-         <form class="form_profil" action="profil.php" id="profil" method="post" enctype="multipart/form-data">
-           <h1> Mes informations </h1>
-           <div class="img_profil">
-             <img src="img/<?= $utilisateur->getImage(); ?>" alt="Photo de profil">
-             <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" value="" style='color:lightgrey'>
-           </div>
-           <div class="label_input_profil">
-             <label for="pseudo">Pseudo </label>
-             <input type="text" name="pseudo" id="pseudo" placeholder="<?= $utilisateur->getPseudo(); ?>">
-           </div>
+        <form class="form_profil" action="profil.php" id="profil" method="post" enctype="multipart/form-data">
+          <h1> Mes informations </h1>
+          <div class="img_profil">
+            <img src="img/<?= $utilisateur->getImage(); ?>" alt="Photo de profil">
+            <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" value="" style='color:lightgrey'>
+          </div>
+          <div class="label_input_profil">
+            <label for="pseudo">Pseudo </label>
+            <input type="text" name="pseudo" id="pseudo" placeholder="<?= $utilisateur->getPseudo(); ?>">
+          </div>
 
-           <div class="label_input_profil">
-             <label for="email">Adresse e-mail </label>
-             <input type="text" name="email" id="email" placeholder="<?= $utilisateur->getEmail(); ?>">
-           </div>
+          <div class="label_input_profil">
+            <label for="email">Adresse e-mail </label>
+            <input type="text" name="email" id="email" placeholder="<?= $utilisateur->getEmail(); ?>">
+          </div>
 
-           <div class="label_input_profil">
-             <label for="password">Mot de passe</label>
-             <input type="password" id="password" name="password">
-           </div>
+          <div class="label_input_profil">
+            <label for="password">Mot de passe</label>
+            <input type="password" id="password" name="password">
+          </div>
 
-           <div class="label_input_profil">
-             <label for="confirm_password"> Confirmer le mot de passe</label>
-             <input type="password" id="confirm_password" name="confirm_password">
-           </div>
+          <div class="label_input_profil">
+            <label for="confirm_password"> Confirmer le mot de passe</label>
+            <input type="password" id="confirm_password" name="confirm_password">
+          </div>
 
-           <button type="submit" class="button_pages" name="submit"> Modifier </button>
+          <button type="submit" class="button_pages" name="submit"> Modifier </button>
 
-         </form>
+        </form>
      </main>
 
      <?php include('views/footer.php'); ?>
    </body>
+   <script src="ressources/js/formulaires.js" charset="utf-8"></script>
 </html>
